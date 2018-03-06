eb7d5239b0d5635b976371c3971b715e1ffc253b
鸿展 赖 <laihongzhan1998@hotmail.com>
2018-03-06 16:23:23 +0800

    add interface

--- BigDataSolver/main.cpp
+++ BigDataSolver/main.cpp
@@ -1,11 +1,13 @@
 #include "main.h"
 using namespace std;
 
-int main(void) {
-	//第一题开始
-	ifstream file_in("user.txt", ios::in);
+void questionOne() {
+	ifstream file_in("user.txt", ios::in);		
 	ofstream file_out("password.txt", ios::out);
-	if (!file_in) return 0;
+	if (!file_in) {
+		cout << "WARING: read \'user.txt\' wrong" << endl;
+		return;
+	}
 	int i = 0;
 	while (!file_in.eof()) {
 		string one_line;
@@ -13,15 +15,16 @@ int main(void) {
 		user[i].password = one_line.substr(one_line.find("\t") + 1);
 		i++;
 	}
-	/*for (int i = 0; i < 1230000; i++) {
-	cout << user[i].password << endl;
-	}*/
+
 	for (int i = 0; i < 1230000; i++) {
 		counter[i].password = "\0";
 	}
 	cout << 11 << endl;
 	for (int i = 0; i < 1230000; i++) {//user
-		if (i % 100000 == 0)cout << i << endl;
+		if (i % 10000 == 0) {
+			system("cls");
+			cout << i / 12300 << "%" << endl;
+		}
 		for (int j = 0; j < 1230000; j++) {//counter
 			if (user[i].password.compare(counter[j].password) == 0) {
 				counter[j].count++;
@@ -40,11 +43,20 @@ int main(void) {
 			}
 		}
 	}
-	if (!file_out)return 0;
+	if (!file_out) {
+		cout << "WARING: write \'password.txt\' wrong" << endl;
+		return;
+	}
 	int a = 0;
 	while (counter[a].count>0) {
 		file_out << counter[a].password << "\t" << counter[a].count << endl;
 		a++;
 	}
 	cout << 111;
+}
+
+int main(void) {
+	//第一题开始
+	questionOne();
+	
 }
